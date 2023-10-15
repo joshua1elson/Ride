a6a5473e1384868ed19ebf43a6012c342f31a791
HonJosh <joshua1elsonhonu@gmail.com>
2023-10-15 13:16:35 +0000

    first edit

---
+++ App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import DriverScreen from './screens/DriverScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RegistrationScreen from './screens/RegistrationScreen';
+
+
+
+//set up redux
+
+export default function App() {
+  
+const Stack = createNativeStackNavigator();
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        
+        <SafeAreaProvider>
+          
+          <Stack.Navigator>
+            <Stack.Screen name="RegistrationScreen" 
+             component={RegistrationScreen} 
+             options={{
+                headerShown:true,
+             }}
+            />
+            <Stack.Screen name="HomeScreen" 
+             component={HomeScreen} 
+             options={{
+                headerShown:false,
+             }}
+            />
+            
+            <Stack.Screen name="MapScreen" 
+             component={MapScreen} 
+             options={{
+                headerShown:false,
+             }}
+            />
+            <Stack.Screen name="DriverScreen" 
+             component={DriverScreen} 
+             options={{
+                headerShown:false,
+             }}
+            />
+          </Stack.Navigator> 
+        </SafeAreaProvider> 
+      </NavigationContainer>
+      
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
